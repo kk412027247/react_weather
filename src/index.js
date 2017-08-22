d95d08f39eb47cea8450be0bdc63d67be33f23ec
tmd <123456>
2017-08-22 21:18:25 +0800

    不知道哪里对了

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Weather from './Weather';
-import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<Weather />, document.getElementById('root'));
-registerServiceWorker();
